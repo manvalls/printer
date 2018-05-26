00b6bfa6d84d4c128e61c787b1e62e0931a2cee1
Manuel Valls Fernández <manolo@vlrz.es>
2018-05-26 18:00:00 +0200

    Workaround for blank stripes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"image"
 	"log"
 	"os"
 
@@ -11,42 +12,73 @@ import (
 
 const maxChunkHeight = 400
 
+func isBlankLine(y int, img *image.Gray, bounds image.Rectangle) bool {
+
+	for x := 0; x < bounds.Max.X; x++ {
+		if img.GrayAt(x, y).Y == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func nextBreakPoint(offset int, img *image.Gray, bounds image.Rectangle, maxChunkHeight int) int {
+	chunkHeight := bounds.Max.Y - offset
+	if chunkHeight > maxChunkHeight {
+		chunkHeight = maxChunkHeight
+	}
+
+	maxConsecutiveBlankRows := 0
+	breakpoint := offset + chunkHeight
+	currentBlankRows := 0
+
+	for y := 0; y < chunkHeight; y++ {
+		if isBlankLine(y+offset, img, bounds) {
+			currentBlankRows++
+			if currentBlankRows > maxConsecutiveBlankRows {
+				maxConsecutiveBlankRows = currentBlankRows
+				breakpoint = y + offset + 1
+			}
+		} else {
+			currentBlankRows = 0
+		}
+	}
+
+	return breakpoint
+}
+
 func main() {
 
 	// Init
 	buffer := make([]byte, 0, 10e3)
-	buffer = append(buffer, 0x1b, 0x40)
+	buffer = append(buffer, 0x1b, 0x40, 0x1b, 0x33, 50)
 
 	img, err := halfgone.LoadImage(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if img.Bounds().Max.X >= 576 {
-		img = imaging.Resize(img, 576, 0, imaging.Lanczos)
-	}
-
+	img = imaging.Resize(img, 576, 0, imaging.Lanczos)
 	gray := halfgone.ImageToGray(img)
 	dithered := halfgone.FloydSteinbergDitherer{}.Apply(gray)
 
 	bounds := dithered.Bounds()
+
 	roundedX := (bounds.Max.X + (bounds.Max.X % 8)) / 8
 	bytesWidth := make([]byte, 2)
 	binary.LittleEndian.PutUint16(bytesWidth, uint16(roundedX))
 
 	offsetY := 0
 	for offsetY < bounds.Max.Y {
-		chunkHeight := bounds.Max.Y - offsetY
-		if chunkHeight > maxChunkHeight {
-			chunkHeight = maxChunkHeight
-		}
+		breakpoint := nextBreakPoint(offsetY, dithered, bounds, maxChunkHeight)
 
 		bytesHeight := make([]byte, 2)
-		binary.LittleEndian.PutUint16(bytesHeight, uint16(chunkHeight))
+		binary.LittleEndian.PutUint16(bytesHeight, uint16(breakpoint-offsetY))
 
 		buffer = append(buffer, 0x1d, 0x76, 0x30, 0x00, bytesWidth[0], bytesWidth[1], bytesHeight[0], bytesHeight[1])
 
-		for y := 0; y < chunkHeight; y++ {
+		for y := offsetY; y < breakpoint; y++ {
 			for x := 0; x < roundedX; x++ {
 				b := byte(0)
 
@@ -56,7 +88,7 @@ func main() {
 					if x*8+ix > bounds.Max.X {
 						value = 0
 					} else {
-						pixel := dithered.GrayAt(x*8+ix, offsetY+y)
+						pixel := dithered.GrayAt(x*8+ix, y)
 						if pixel.Y == 0 {
 							value = 1
 						} else {
@@ -71,8 +103,7 @@ func main() {
 			}
 		}
 
-		// buffer = append(buffer, 10)
-		offsetY += chunkHeight
+		offsetY = breakpoint
 	}
 
 	// Cut & end
